66baee03cea412dec2ee75505c4e4c3b9cf3f360
spaceInvader <sviat.gotsko@gmail.com>
2020-09-06 14:26:49 +0300

    HomePage edited

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,21 +2,43 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FreeForum.ViewModels;
+using System.Collections.Generic;
+using FreeForum.ViewModels.Post;
+using FreeForum.Data;
+using System.Linq;
 
 namespace FreeForum.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IPost _postService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IPost postService)
         {
             _logger = logger;
+            _postService = postService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<PostListingModel> posts = _postService.GetAllByNew()
+                .Select(post => new PostListingModel
+                {
+                    Id = post.Id,
+                    Title = post.Title,
+                    UserName = post.User.UserName,
+                    DateCreated = post.Created.ToString(),
+                    Content = post.Content,
+                    RepliesCount = post.Replies.Count()
+                });
+
+            var model = new PostIndexModel
+            {
+                PostList = posts
+            };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
